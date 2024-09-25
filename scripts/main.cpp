51dfafb507a5d6e6bb7b36a2d317ecad7f2aeaff
Karscoding <97431994+Karscoding@users.noreply.github.com>
2024-09-25 11:26:04 +0200

    Basic image loading

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -2,51 +2,55 @@
 #include <SDL_image.h>
 
 
-#define WIDTH 800
-#define HEIGHT 600
-#define IMG_PATH "/girlypop.png"
+#define WIDTH 1280
+#define HEIGHT 720
 
 int main(int argc, char* args []) {
     // variable declarations
     SDL_Window *win = NULL;
     SDL_Renderer *renderer = NULL;
 
+    bool running = false;
+
     // Initialize SDL.
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
         return 1;
 
-    IMG_Init(IMG_INIT_PNG);
 
     // create the window and renderer
     // note that the renderer is accelerated
-    win = SDL_CreateWindow("Image Loading", 100, 100, WIDTH, HEIGHT, 0);
+    win = SDL_CreateWindow("Train Simulator", 100, 100, WIDTH, HEIGHT, 0);
     renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Surface * image = IMG_Load("girlypop.png");
-    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+    SDL_Texture * texture = IMG_LoadTexture(renderer, "../resources/VIRM.png");
+    SDL_Rect destination;
+    destination.h = 200;
+    destination.w = 1000;
+    destination.x = -500;
+    destination.y = 380;
+
+    running = true;
 
     // main loop
-    while (1) {
+    while (running) {
 
         // event handling
         SDL_Event e;
         if ( SDL_PollEvent(&e) ) {
-            if (e.type == SDL_QUIT)
-                break;
-            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE)
-                break;
+            if (e.type == SDL_QUIT || e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE) {
+                running = false;
+            }
         }
 
         // clear the screen
         SDL_RenderClear(renderer);
         // copy the texture to the rendering context
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderCopy(renderer, texture, NULL, &destination);
         SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
-    IMG_Quit();
     SDL_Quit();
     return 0;
 }
